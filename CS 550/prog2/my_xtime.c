efecccab20e940f29bed5e503c5d32057eb104e9
Zach Seymour <zseymou1@zseymou1.localdomain>
2014-03-29 22:10:04 +0000

    Updating

--- CS 550/prog2/my_xtime.c
+++ CS 550/prog2/my_xtime.c
@@ -1,9 +1,13 @@
 asmlinkage int my_xtime(struct timespec *current_time)
 {
-   if(!access_ok(VERIFY_WRITE, current_time, sizeof(struct timespec)))
-	return -EFAULT;
-
-   copy_to_user(current_time, &timekeeper.xtime, sizeof(struct timespec));
-   return 0;
+        struct timespec now;
+        int ret = 0;
+        //if(!access_ok(VERIFY_WRITE, current_time, sizeof(now)))
+        //      return -EFAULT;
+        getnstimeofday(&now);
+        ret = copy_to_user(current_time, &now, sizeof(now));
+        printk(KERN_ALERT "It is now %ld nanoseconds.\n", now.tv_nsec);
+        return ret;
 }
 EXPORT_SYMBOL(my_xtime);
+
--- CS 550/prog2/use_mycall.c
+++ CS 550/prog2/use_mycall.c
@@ -1,16 +1,16 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
 #include <linux/unistd.h>
-#include <linux/time.h>
-#define __NR_sys_my_xtime 333
+#define __NR_my_xtime 314
 
 int main() {
    int ret;
-   struct timespec *current_time;
-   ret = syscall(__NR_sys_my_xtime, current_time);
-   if(ret)
-      printf("Time is %ld ns.\n", current_time.tv_nsec);
+   struct timespec *current_time = malloc(sizeof(struct timespec));
+   ret = syscall(__NR_my_xtime, current_time);
+   if(ret==0)
+      printf("Time is %ld ns.\n", current_time->tv_nsec);
    else
       printf("ret = %d, errno = %d\n", ret, errno);
    return 0;
